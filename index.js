3947e0ce95ad28396a5fab7e513303c64eb2360f
StevenLove <lovestevend@gmail.com>
2015-07-27 23:42:00 -0400

    autoscroll and facebook login

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/facebook.js',function(req,res){
+  res.sendFile(__dirname + '/facebook.js');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
 
