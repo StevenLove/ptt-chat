c1841fc40cebc33f5da9685f2c8864b54face12d
StevenLove <lovestevend@gmail.com>
2015-09-07 20:47:04 -0400

    got stupid options working for synonymization

--- Transformer.js
+++ Transformer.js
@@ -95,6 +95,7 @@ var Transformer = function(){
     });
     return thumb_urls;
   }
+
   var BingImage = function(text, options, callback){
     var bing_options =  {top: 3, market: 'en-US'};
     Bing.images(
@@ -156,11 +157,12 @@ var Transformer = function(){
     const api_key = ***REMOVED***;
     const format = "json";
     var api_url = api_url_base+api_key+"/"+word+"/"+format;
-    var options = {
-      url: api_url,
-    }
+    // var bhs_opt = {
+    //   url: api_url,
+    // }
     request(
-      options, callback
+      {"url": api_url},
+      callback
     );
   }
 
@@ -172,52 +174,65 @@ var Transformer = function(){
     return Object.keys(dict);
   }
 
-  var ParseBigHugeSynonymList = function(body){
-    if(!body){
-      return [];
-    }
-    var results = JSON.parse(body);
-    var total = [];
-    Object.keys(results).forEach(
-      function(part_of_speech){
-        var pos_specified_word = results[part_of_speech]
-        var syn = pos_specified_word["syn"];
-        var sim = pos_specified_word["sim"];
-        var both = syn.concat(sim);
-        total = total.concat(both);
-      }
-    );
-    var synonyms = RemoveDuplicatesAndFalsies(total);
-    return synonyms;
-  }
-
+  // raw: just return the output of the bighugethesaurus API
+  // pos: only words in this part of speech will be returned
+  // ant: only antonyms will be returned
   var ApplySynonymizeOptions = function(body, options){
+    // console.log("APPLYBODY: " + body.slice(0,20));
+    // console.log("APPLYOPT: " + JSON.stringify(options));
     var results = [];
+    if(body){ body = JSON.parse(body); } 
+    else{ return []; }
     if(options.raw){
-
-      results = (body)? JSON.parse(body) : {} ;
-      // if(!body){
-      //   results = {};
-      // }
-      // else{
-      //   results = JSON.parse(body);
+      results = body;
+      return results;
+    }
+    var syn = [];
+    var sim = [];
+    var ant = [];
+    var candidates = [];
+    if(options.pos){
+      if(body[options.pos]){
+        syn = syn.concat(body[options.pos]["syn"]);
+        sim = sim.concat(body[options.pos]["sim"]);
+        ant = ant.concat(body[options.pos]["ant"]);
+      }
     }
     else{
-      results = ParseBigHugeSynonymList(body);
-      if(options.include_base){
-        results.unshift(options.text);
-      }
+      Object.keys(body).forEach(function(pos){
+        syn = syn.concat(body[pos]["syn"]);
+        sim = sim.concat(body[pos]["sim"]);
+        ant = ant.concat(body[pos]["ant"]);
+      });
     }
+    candidates = (options.ant)? ant : syn.concat(sim);
+    results = RemoveDuplicatesAndFalsies(candidates);
     return results;
   }
 
+  function logOptions(a){
+    return function(err, response, body){
+      var op = a;
+      log(err,response,body);
+      console.log(op);
+    }
+  }
+
+  function log(err, response, body){
+    console.log(err);
+    console.log(body);
+  }
+
+
   var Synonymize = function(text, options, callback){
+    console.log("Synonymizing " + text + " with options " + JSON.stringify(options));
+
     ConsumeBigHugeSynonymAPI(
       text,
       function(err, result, body){
         var success = {};
         if(!err){
-         success = ApplySynonymizeOptions(body,options);
+          success = ApplySynonymizeOptions(body,options);
         }
         callback(err,success);
       }
@@ -298,54 +313,49 @@ var Transformer = function(){
     );
   }
 
-  function GetSynonymLists(sentence, callback){
-    var synonym_lists = [];
-    var counter = sentence["words"].length;
 
+
+  var Clone = function(obj){
+    var result = {};
+    Object.keys(obj).forEach(function(key){
+      result[key] = obj[key];
+    });
+    return result;
+  }
+
+
+
+  function GetSynonymLists(sentence, options, callback){
+    var synonym_lists = [];
+    var count_current = 0;
+    var count_total = sentence.words.length;
+    var result = {};
+    var synonym_options_list = [];
     async.forEachOf(
       sentence.words,
-      function(word, index, callback){
+      function(word, index, cb){
         var lay_part = sentence.lay_parts[index];
+        synonym_options_list[index] = Clone(options);
+        synonym_options_list[index]["pos"] = lay_part;
+        synonym_options_list[index]["text"] = word;
         Synonymize(
-          word, 
-          {"raw":true, "text":word,"mode":"synonymize"},
+          word,
+          synonym_options_list[index],
           function(err, success){
             if(err){
-              callback(err);
+              cb(err);
             }
-            var syn_list = [];
-            if(success[lay_part]){
-              var syn_list = success[lay_part]["syn"];
+            else{
+              synonym_lists[index] = success;
             }
-            syn_list.unshift(word);
-            synonym_lists[index] = syn_list; 
-            callback();
+            cb();
           }
-        )
+        );
       },
       function(err){
         callback(err,synonym_lists);
       }
     );
-
-
-    // sentence.words.forEach(function(word, index){
-    //   var lay_part = sentence.lay_parts[index];
-
-    //   GetBigHugeSynonymList(word, function(bighugeresult){
-    //     // console.log(bighugeresult);
-    //     // var syn_list = GetSamePosSynonyms(bighugeresult, lay_part);
-    //     var syn = bighugeresult[lay_part];
-    //     syn_list.unshift(word);
-    //     // console.log(syn_list);
-    //     synonym_lists[index]=syn_list;
-    //     // synonym_lists[index] = GetSamePosSynonyms(bighugeresult, lay_part);
-    //     --counter;
-    //     if(counter == 0){
-    //       callback(synonym_lists);
-    //     }
-    //   })
-    // });
   }
 
   var SmartSynonymize = function(text, options, output_callback){
@@ -353,62 +363,18 @@ var Transformer = function(){
       [
         function(callback) {
             console.log("async1");
-            PartsOfSpeechify(text,options,callback);
-            // callback(null, sentence)
+            PartsOfSpeechify(text, options, callback);
         },
         function(sentence, callback) {
-            console.log("async2" + JSON.stringify(sentence));
-            GetSynonymLists(sentence, callback);
+            console.log("async2 " + JSON.stringify(sentence));
+            GetSynonymLists(sentence, options, callback);
         }
       ],
       function(err, result){
         output_callback(err,result);
       }
     );
-    // GetPartsOfSpeech(
-    //   text,
-      // function(result){
-        // var sentence = ParsePartOfSpeechTaggedText(result);
-        // console.log(sentence);
-        // GetSynonymLists(sentence, function(synonym_lists){
-        //   res.end(JSON.stringify(synonym_lists));
-      //   })
-      // }
-    // );
   }
-
-  function GetSamePosSynonyms(bighugeresult, lay_part){
-    var word = bighugeresult[lay_part];
-    var results=[];
-    var synonyms;
-    var similars;
-    if(word){
-      if(word["syn"]){
-        synonyms = word["syn"];
-      }
-      else{
-        synonyms = [];
-      }
-      if(word["sim"]){
-        similars = word["sim"];
-      }
-      else{
-        similars = [];
-      }
-      results = synonyms.concat(similars);
-    }
-    else{
-      results = [];
-    }
-
-    // maybe i should remove duplicates
-
-    return results;
-  }
-
-
-
-
 
 
   function GetRandomElement(array){
@@ -421,33 +387,7 @@ var Transformer = function(){
       return array[index];
     }
   }
-  // function GetPartsOfSpeech(sentence, callback){
-  //   const api_url = "http://text-processing.com/api/tag/";
-  //   const params = querystring.stringify({
-  //     'text': sentence,
-  //     'output': "tagged"
-  //   });
-  //   const options = {
-  //     url: api_url,
-  //     body: params
-  //   }
-  //   const api_callback = function(error, response, body){
-  //     if(error) {
-  //       console.log(error);
-  //       return;
-  //     }
-  //     var parts_of_speech = JSON.parse(body)["text"];
-  //     callback(parts_of_speech);
-  //   }
-  //   request.post(options,api_callback);
 
-  // }
-
-  
-
-
-
-  
 
   function GetWordnetSynonymList(word, callback){
       var wordnet = new WordNet();
